3a1a26d8f731d393992ba105e6487673dd2a1aef
ajscodes <ayushmaradia@gmail.com>
2025-07-19 13:29:46 +0530

    update

--- lab_5_searching.c
+++ lab_5_searching.c
@@ -1,14 +1,28 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<time.h>
+#include<stdbool.h>
 
 void LinearSearch(){
 
 }
 
-void BinarySearch(){
-    
+//void BinarySearch(){
+
 }
 
 int main(){
-    
+  int number_of_element_in_array;
+  int element_to_search;
+
+  FILE *file;
+  clock_t start, end;
+  double cpu_time;
+
+  printf("Enter n number of elements: ");
+  scanf("%d", &number_of_element_in_array);
+
+  printf("Enter element to search: ");
+  scanf("%d", &element_to_search);
+
 }
